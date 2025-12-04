e02bef0cf39eba603e216bc5c148728a67d5732b
RAIN-QA <rain1702@mail.ru>
2025-12-04 19:36:18 +0700

    Pytest skip, skipif, xfail

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,8 @@
+import pytest
 from playwright.sync_api import sync_playwright, expect
 
-
+@pytest.mark.regression
+@pytest.mark.courses
 def test_empty_courses_list():
     # Открываем браузер с использованием Playwright
     with sync_playwright() as playwright:
