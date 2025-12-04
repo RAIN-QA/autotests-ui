2a6111946bcf68d7a90192d339d6052b6e896a9e
RAIN-QA <rain1702@mail.ru>
2025-12-04 17:10:45 +0700

    Маркировки Pytest

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,8 +1,9 @@
+import pytest
 from playwright.sync_api import sync_playwright, expect
 
-
-def test_successful_registration():  # Создаем тестовую функцию
-    # Все остальные действия остаются без изменений
+@pytest.mark.regression
+@pytest.mark.registration
+def test_successful_registration():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
