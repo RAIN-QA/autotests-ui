e770f04584e0700ff586fe9d9132524981d845d2
RAIN-QA <rain1702@mail.ru>
2025-12-09 15:46:03 +0700

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Page, Playwright
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
 def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
         # Передаем страницу для использования в тесте
     yield browser.new_page()
         # Закрываем браузер после выполнения тестов
