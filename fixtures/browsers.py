beeb1298700a0872cbbc10d64788c88f98e42477
RAIN-QA <rain1702@mail.ru>
2025-12-16 07:54:49 +0700

    Знакомство с PageObject

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,14 +1,12 @@
 import pytest  # Импортируем pytest
-from playwright.sync_api import Page, Playwright
+from playwright.sync_api import Playwright, Page
     # Имопртируем класс страницы, будем использовать его для аннотации типов
 
 
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
-    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
-        # Передаем страницу для использования в тесте
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
-        # Закрываем браузер после выполнения тестов
     browser.close()
 
 
