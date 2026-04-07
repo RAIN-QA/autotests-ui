abdb7ff8b56cdd89130a703f7664b6b73e783978
RAIN-QA <rain1702@mail.ru>
2026-04-08 05:52:14 +0700

    Моки и оптимизация UI автотестов

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,6 +1,8 @@
 import allure
 from playwright.sync_api import Playwright, Page
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
         playwright: Playwright,
@@ -11,7 +13,9 @@ def initialize_playwright_page(
     browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url(), storage_state=storage_state, record_video_dir=settings.videos_dir)
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
