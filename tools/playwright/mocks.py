0e466a503f2e8976917ec48a707e1daa5b200651
RAIN-QA <rain1702@mail.ru>
2026-04-14 05:43:26 +0700

    Логирование взаимодействий со страницей

--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, Route
+from playwright.sync_api import Page
 
 def mock_static_resources(page: Page):
     page.route("**/*.{ico,png,jpg,jpeg,gif,svg,webp,bmp,mp3,mp4,woff,woff2}", lambda route: route.abort)
