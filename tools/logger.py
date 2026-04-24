9df0d3aa57d8ca6939554cf8afa55f4a685730bc
RAIN-QA <rain1702@mail.ru>
2026-04-25 06:35:38 +0700

    Запуск автотестов на GitHub Actions

--- tools/logger.py
+++ tools/logger.py
@@ -7,7 +7,7 @@ def get_logger(name: str) -> logging.Logger:
     handler = logging.StreamHandler()
     handler.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
